5bc05cb73746cd95e2455ac0798c96d5553dea02
LunarWanderer <lunarwanderer13@gmail.com>
2026-01-02 21:47:43 +0100

    Change command handling from guild-only to global

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -9,7 +9,7 @@ export default (client: Client): void => {
         }
 
         const rest = new REST().setToken(process.env.TOKEN!)
-        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!), { body: Commands.map(command => command.data.toJSON()) })
+        await rest.put(Routes.applicationCommands(client.user.id), { body: Commands.map(command => command.data.toJSON()) })
 
         console.log(`Logged in as ${client.user.username}`)
     })
