dc3b8c70b07e83569d10d80fa1e48cb1b6ca267a
LunarWanderer <lunarwanderer13@gmail.com>
2026-01-04 03:03:23 +0100

    Add further functionality to getting info about cards and add basic info in major.json and minor.json

--- src/commands/tarot.ts
+++ src/commands/tarot.ts
@@ -1,5 +1,30 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js"
-import { Command } from "./../index"
+import { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder, MessageFlags } from "discord.js"
+import { Command, Color, Emoji, Emojis } from "./../utils/config"
+import fs from "fs"
+
+interface TarotCard {
+    name: string
+    type: "major_arcana" | "minor_arcana"
+    upright_meaning: string,
+    reversed_meaning: string,
+    url: string,
+    query: string
+}
+
+interface MajorArcana extends TarotCard {
+    type: "major_arcana",
+    number: string
+}
+
+interface MinorArcana extends TarotCard {
+    type: "minor_arcana",
+    suit: string,
+    element: string
+}
+
+const major_cards: MajorArcana[] = JSON.parse(fs.readFileSync("src/source/tarot/jsons/major.json", "utf-8"))
+const minor_cards: MinorArcana[] = JSON.parse(fs.readFileSync("src/source/tarot/jsons/minor.json", "utf-8"))
+const all_cards: (MajorArcana | MinorArcana)[] = [...major_cards, ...minor_cards]
 
 export const Tarot: Command = {
     data: new SlashCommandBuilder()
@@ -9,11 +34,33 @@ export const Tarot: Command = {
         
         .addSubcommand(subcommand => subcommand
             .setName("draw")
-            .setDescription("Draws tarot cards (default 1).")
+            .setDescription("Draws a tarot card.")
 
-            .addNumberOption(option => option
-                .setName("cards")
-                .setDescription("The number of cards to draw.")
+            .addStringOption(option => option
+                .setName("range")
+                .setDescription("Which arcana should be used drawn.")
+                .addChoices(
+                    { name: "Major Arcana", value: "major" },
+                    { name: "Minor Arcana", value: "minor" },
+                    { name: "Full Deck", value: "both" }
+                )
+                .setRequired(false)
+            )
+
+            .addStringOption(option => option
+                .setName("face")
+                .setDescription("Which way should the cards face.")
+                .addChoices(
+                    { name: "Upright", value: "upright" },
+                    { name: "Reversed", value: "reversed" },
+                    { name: "Any", value: "both" }
+                )
+                .setRequired(false)
+            )
+
+            .addBooleanOption(option => option
+                .setName("hidden")
+                .setDescription("Whether the message should be hidden or not.")
                 .setRequired(false)
             )
         )
@@ -27,17 +74,82 @@ export const Tarot: Command = {
                 .setDescription("The card to get info about.")
                 .setRequired(true)
             )
+
+            .addBooleanOption(option => option
+                .setName("hidden")
+                .setDescription("Whether the message should be hidden or not.")
+                .setRequired(false)
+            )
         ),
 
-    async execute(interaction: ChatInputCommandInteraction) {
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
         const subcommand: string = interaction.options.getSubcommand()
+        const hidden: boolean = interaction.options.getBoolean("hidden") ?? false
 
         switch (subcommand) {
             case "draw":
-                await interaction.reply("draw")
+                const range: string = interaction.options.getString("range") ?? "major"
+
+                const draw_embed: EmbedBuilder = new EmbedBuilder()
+                    .setColor(Color.primary)
+                    .setTitle(`I draw...`)
+
+                await interaction.reply({ embeds: [draw_embed] })
                 break
             case "info":
-                await interaction.reply("info")
+                const card_option: string | null = interaction.options.getString("card")
+                if (!card_option) break
+                const card: MajorArcana | MinorArcana | undefined = all_cards.find(card => card_option.includes(card.query))
+
+                if (!card) {
+                    await interaction.reply({ content: "I couldn't find the card you asked for, sorry!", flags: MessageFlags.Ephemeral })
+                    break
+                }
+
+                const cards_emoji: Emoji | undefined = Emojis.find(emoji => emoji.name === "cards")
+                if (!cards_emoji) break
+                const cards_emoji_string: string = `<:${cards_emoji.name}:${cards_emoji.id}>`
+
+                const info_embed: EmbedBuilder = new EmbedBuilder()
+                    .setColor(Color.primary)
+                    .setImage(card.url)
+                    .setFields(
+                        {
+                            "name": "Upright meaning:",
+                            "value": card.upright_meaning,
+                            "inline": true
+                        },
+                        {
+                            "name": "Reversed meaning:",
+                            "value": card.reversed_meaning,
+                            "inline": true
+                        }
+                    )
+
+                if (card.type === "major_arcana") {
+                    info_embed.setTitle(`:sparkles: Info about ${card.name} card ${cards_emoji_string}`)
+                }
+
+                if (card.type === "minor_arcana") {
+                    const element_emoji: Emoji | undefined = Emojis.find(emoji => emoji.name === card.element)
+                    if (!element_emoji) break
+                    const element_emoji_string: string = `<:${element_emoji.name}:${element_emoji.id}>`
+
+                    info_embed.setTitle(`:sparkles: Info about the ${card.name} of ${card.suit} card ${cards_emoji_string}`)
+                    info_embed.addFields(
+                        {
+                            "name": "Element:",
+                            "value": `${card.element.charAt(0).toUpperCase()}${card.element.slice(1)} ${element_emoji_string}`,
+                            "inline": false
+                        }
+                    )
+                }
+
+                if (!hidden) {
+                    await interaction.reply({ embeds: [info_embed] })
+                } else {
+                    await interaction.reply({ embeds: [info_embed], flags: MessageFlags.Ephemeral })
+                }
                 break
         }
     }
