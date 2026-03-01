57587c29b86ea9de5f4ccc9acf7af8215cdf2ba2
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-03-01 22:48:33 +0100

    Add the ability to remove commands and add variable types

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -1,15 +1,26 @@
-import { Client, REST, Routes } from "discord.js"
+import { Client, REST, Routes, User } from "discord.js"
 import { Commands } from "./../index"
 import "dotenv/config"
 
 export default (client: Client): void => {
     client.on("clientReady", async () => {
-        if (!client.user || !client.application) {
-            return
+        if (!client.user || !client.application) return
+
+        const app: User = client.user
+        const restart: boolean = false
+        const rest: REST = new REST().setToken(process.env.TOKEN!)
+
+        if (restart) {
+            // Remove global commands
+            await rest.put(Routes.applicationCommands(app.id),  { body: [] })
+
+            // Remove guild commands
+            client.guilds.cache.forEach(async guild => {
+                await rest.put(Routes.applicationGuildCommands(app.id, guild.id), { body: [] })
+            })
         }
 
-        const rest = new REST().setToken(process.env.TOKEN!)
-        await rest.put(Routes.applicationCommands(client.user.id), { body: Commands.map(command => command.data.toJSON()) })
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), { body: Commands.map(command => command.data.toJSON()) })
 
         console.log(`Logged in as ${client.user.username}`)
     })
