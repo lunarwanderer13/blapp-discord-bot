2c5a45c44aa94f480fc8d0b8783bae24e7e789e0
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-03-29 22:25:09 +0200

    Add /action pet with only text for now

---
+++ src/commands/action.ts
@@ -0,0 +1,38 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, User, AttachmentBuilder, EmbedBuilder } from "discord.js"
+import { Command, Color } from "./../utils/config"
+import fs from "fs"
+
+export const Action: Command = {
+    data: new SlashCommandBuilder()
+        .setName("action")
+        .setDescription("Perform an action.")
+        .setContexts([0, 1, 2])
+
+        .addSubcommand(subcommand => subcommand
+            .setName("pet")
+            .setDescription("Pet someone.")
+
+            .addUserOption(option => option
+                .setName("target")
+                .setDescription("User to pet.")
+                .setRequired(false)
+            )
+        ),
+
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+        const subcommand: string = interaction.options.getSubcommand()
+
+        switch(subcommand) {
+            case "pet":
+                const target: User | null = interaction.options.getUser("target")
+
+                const pet_embed: EmbedBuilder = new EmbedBuilder()
+                    .setColor(Color.primary)
+
+                if (target) pet_embed.setTitle(`${interaction.user} pets ${target}!`)
+                else pet_embed.setTitle(`${interaction.user} pets you!`)
+
+                await interaction.reply({ embeds: [pet_embed] })
+        }
+    }
+}
