011672b2e2c91913786089aa76315ce5749c8246
LunarWanderer月 <76808845+lunarwanderer13@users.noreply.github.com>
2026-05-23 13:44:23 +0200

    Add /action (#3)
    * Add /action pet with only text for now
    
    * Add action to index.ts
    
    * Bump version
    
    * Add /action kiss
    
    * Add ui for /action kiss
    
    * Replace mentions in embed title
    
    * Sort actions alphabetically ascending
    
    * Massively refactor and optimize the command
    
    * Add replies for /action pet
    
    * Add /action punch
    
    * Add /action hug
    
    * Add gifs
    
    * Add /action blush
    
    * e a sports. its in the game.
    
    * Remove unnecessary axios import
    
    * Bump version
    
    * Add anime name to action embeds
    
    * Add artist credit
    
    * Add documentation comments

---
+++ src/commands/action.ts
@@ -0,0 +1,110 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, User, AttachmentBuilder, EmbedBuilder } from "discord.js"
+import { fetchRandom, NbIndividualResponse, NbCategories } from "nekos-best.js"
+import { Command, Color, sendError } from "./../utils/config"
+import fs from "fs"
+
+interface ActionReplies {
+    targetted?: string[]
+    unspecified: string[]
+}
+
+export const Action: Command = {
+    data: new SlashCommandBuilder()
+        .setName("action")
+        .setDescription("Perform an action.")
+        .setContexts([0, 1, 2])
+
+        .addSubcommand(subcommand => subcommand
+            .setName("blush")
+            .setDescription("Show embarrassment.")
+        )
+
+        .addSubcommand(subcommand => subcommand
+            .setName("hug")
+            .setDescription("Hug someone.")
+
+            .addUserOption(option => option
+                .setName("target")
+                .setDescription("User to hug.")
+                .setRequired(false)
+            )
+        )
+
+        .addSubcommand(subcommand => subcommand
+            .setName("kiss")
+            .setDescription("Kiss someone.")
+
+            .addUserOption(option => option
+                .setName("target")
+                .setDescription("User to kiss.")
+                .setRequired(false)
+            )
+        )
+
+        .addSubcommand(subcommand => subcommand
+            .setName("pat")
+            .setDescription("Pat someone.")
+
+            .addUserOption(option => option
+                .setName("target")
+                .setDescription("User to pat.")
+                .setRequired(false)
+            )
+        )
+
+        .addSubcommand(subcommand => subcommand
+            .setName("punch")
+            .setDescription("Punch someone.")
+
+            .addUserOption(option => option
+                .setName("target")
+                .setDescription("User to punch.")
+                .setRequired(false)
+            )
+        ),
+
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+        // Get the requested action to execute
+        const subcommand: string = interaction.options.getSubcommand()
+
+        const user: User = interaction.user // The person executing the command
+        const target: User | null = interaction.options.getUser("target") ?? null // The person being targetted by the command (optional)
+
+        // Gif returned by the nekos-best.js API
+        const gif: NbIndividualResponse = (await fetchRandom(subcommand as NbCategories)).results[0]
+
+        // Make sure the response is valid
+        if (!gif) {
+            sendError(interaction, "GIF not found")
+            return
+        }
+
+        // Turn the gif into a discord attachment
+        const attchment: AttachmentBuilder = new AttachmentBuilder(gif.url, { name: `${subcommand}.gif` })
+
+        // Get a randomized reply to the action
+        const action_replies: ActionReplies = JSON.parse(fs.readFileSync(`src/source/action/${subcommand}.json`, "utf-8"))
+        let reply: string
+
+        if (target && action_replies.targetted) reply = action_replies.targetted[Math.floor(Math.random() * action_replies.targetted.length)]
+        else reply = action_replies.unspecified[Math.floor(Math.random() * action_replies.unspecified.length)]
+
+        // Replace reply tags with the user and target
+        reply = reply.replace("<user>", `**${user.displayName}**`)
+        if (target) reply = reply.replace("<target>", `**${target.displayName}**`)
+
+        // Discord embed to be sent
+        const embed: EmbedBuilder = new EmbedBuilder()
+            .setColor(Color.primary)
+            .setTitle(reply)
+            .setImage(`attachment://${subcommand}.gif`)
+
+        // Gif credit info
+        if (gif.artist_name) embed.setAuthor({ name: `Artist: ${gif.artist_name}` })
+        if (gif.artist_name && gif.artist_href) embed.setAuthor({ name: `Artist: ${gif.artist_name}`, url: gif.artist_href })
+        if (gif.anime_name) embed.setFooter({ text: `Anime: ${gif.anime_name}`})
+
+        // Send the reply
+        await interaction.reply({ embeds: [embed], files: [attchment] })
+    }
+}
