1d74e6993f74c03481d1f36ee675e0264e0e20ca
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-06-08 11:10:54 +0200

    Add /action cry

--- src/commands/action.ts
+++ src/commands/action.ts
@@ -20,6 +20,11 @@ export const Action: Command = {
         )
 
         .addSubcommand(subcommand => subcommand
+            .setName("cry")
+            .setDescription("Show sadness.")
+        )
+
+        .addSubcommand(subcommand => subcommand
             .setName("hug")
             .setDescription("Hug someone.")
 
